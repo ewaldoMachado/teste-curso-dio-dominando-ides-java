b665b73d49a6062de9830bd2e59c98d924a1c593
Ewaldo <ewaldo2016@gmail.com>
2023-04-19 13:45:55 -0300

    Criação de calculadora

---
+++ src/main/java/com/dio/calculadora.java
@@ -0,0 +1,41 @@
+package com.dio;
+
+import java.util.Scanner;
+
+public class calculadora {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b;
+		
+		System.out.println("Digite o valor de a: ");
+		a = scan.nextInt();
+		System.out.println("Digite o valor de b ");
+		b = scan.nextInt();
+
+		int soma = soma(a, b);
+		int subtracao = subtracao(a, b);
+		int divisao = divisao(a, b);
+		int multiplicacao = multiplicacao(a, b);
+		
+		System.out.println(soma);
+		System.out.println(subtracao);
+
+		System.out.println(multiplicacao);
+	}
+	
+	public static int soma(int a, int b){
+		return a + b;
+	}
+	public static int subtracao(int a, int b){
+		return a - b;
+	}
+	public static int divisao(int a, int b){
+		return a / b;
+	}
+	public static int multiplicacao(int a, int b){
+		return a * b;
+	}
+}
